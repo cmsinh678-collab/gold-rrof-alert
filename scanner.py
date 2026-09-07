257961599409d21c4c744ddae15c58bd68862404
cmsinh678-collab <cmsinh678@gmail.com>
2026-09-07 23:26:18 +0700

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -435,15 +435,15 @@ def build_message(signal, current, symbol):
 
     message = (
         f"{direction} <b>{symbol}</b>\n\n"
-        f"📊 Source: OKX\n"
+        #f"📊 Source: OKX\n"
         f"⏱ Timeframe: {TIMEFRAME}\n\n"
         f"💰 Price: {current['close']:.2f}\n"
-        f"📊 Volume: {current['volume']:,.4f}\n\n"
-        f"RROF: {current['RROF']:.2f}\n"
-        f"RROF Smooth: {current['RROF_S']:.2f}\n"
-        f"Signal: {current['SIGNAL']:.2f}\n\n"
-        f"🕐 Candle:\n{current['timestamp']}\n\n"
-        f"🔔 {cross}"
+        #f"📊 Volume: {current['volume']:,.4f}\n\n"
+       # f"RROF: {current['RROF']:.2f}\n"
+       # f"RROF Smooth: {current['RROF_S']:.2f}\n"
+       # f"Signal: {current['SIGNAL']:.2f}\n\n"
+       # f"🕐 Candle:\n{current['timestamp']}\n\n"
+        #f"🔔 {cross}"
     )
 
     return message
